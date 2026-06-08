af41fa065de76ebe18676711e87c446156b52314
ItsAtharv2505 <athravtomar2505@gmail.com>
2026-06-08 11:59:45 -0400

    Add files via upload

---
+++ 7PartsToMyRoutineAgain.py
@@ -0,0 +1,21 @@
+print("My Daily Routine")
+
+#During the school year I used to wake up at 6:30 am to freshen up, and get ready for school.
+
+print("Ever since summer break started, I wake up at 7:30 am to get ready")
+
+#Again, during the school year I used to quickly take a shower, do all of my utilities and leave for school with a banana in my hand.
+
+print("Nowadays after I wake up, I tend to do my utilities, then go for a jog with my dad to come back and take a shower.")
+
+print("After the shower I go downstairs to make breakfast consisting of a tall glass of milk, 2 sunny-side up eggs, and some fruit.")
+
+print("From 9:30 to 10:30 I read one of my books, whether fiction, nonfiction, educational, or entertaining.")
+
+print("Once my reading session is over, I watch TV for 2 hours.")
+
+print("After watching TV, I debug my brain by going outside to hang out with friends.")
+
+print("Finally, I come back inside, have lunch, and that is the end of my morning/afternoon routine.")
+
+print("Additionally, total hours I study for in a week are:", 4 * 7, "hours")
